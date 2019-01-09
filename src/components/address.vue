591011615116e7576b0840246d0e99b399582bf8
Chris Ziogas <ziogaschr@gmail.com>
2019-01-09 17:25:20 +0200

    Apply default editor beautify on src files (for now)

--- src/components/address.vue
+++ src/components/address.vue
@@ -1,237 +1,220 @@
 <template>
-<div id="block_wrapper" v-bind:class="{ active: isAddressActive }">
-  <h1>Address</h1> 
-  
-  <div class="panel">
-    <div class="twocol">
-      <span class="address">{{addressData.address}}</span>
-    </div>
-     <div class="twocol right">
-      <span class="balanceLabel">Balance</span>
-      <span class="balance">{{balance}}</span>
-      <small>ebakus</small>
-     
-    </div>
-    <div class="chart_wrapper">
-      <Chart v-if="chartDataLoaded" :chartData="balanceData" :height="300"></Chart>
-    </div>
-     
-  </div>
-  <div class="panel">
-    <h2>Details</h2>
-        <table>
-      <tr>
-        <td>Balance</td>
-        <td> {{balance}} </td>
-      </tr>
-      <tr>
-        <td>Total in</td>
-        <td>{{weiToEbk(addressData.total_in)}}</td>
-      </tr>
-      <tr>
-        <td>Total out</td>
-        <td >{{weiToEbk(addressData.total_out)}}</td>
-      </tr>
-      <tr>
-        <td>Block rewards</td>
-        <td>{{weiToEbk(addressData.block_rewards)}}</td>
-      </tr>
-      <tr>
-        <td>Total txs</td>
-        <td >{{totalTx}}</td>
-      </tr>
-    </table>
-  </div>
-   <div class="panel">
-    <h2>Transactions</h2>
-        <transactions v-bind:txs="txs" v-bind:address="addressData.address" v-bind:maxOffset="totalTx" v-bind:isTransactions="{active:true }" />
+  <div id="block_wrapper" v-bind:class="{ active: isAddressActive }">
+    <h1>Address</h1>
 
+    <div class="panel">
+      <div class="twocol">
+        <span class="address">{{addressData.address}}</span>
+      </div>
+      <div class="twocol right">
+        <span class="balanceLabel">Balance</span>
+        <span class="balance">{{balance}}</span>
+        <small>ebakus</small>
+      </div>
+      <div class="chart_wrapper">
+        <Chart v-if="chartDataLoaded" :chartData="balanceData" :height="300"></Chart>
+      </div>
+    </div>
+    <div class="panel">
+      <h2>Details</h2>
+      <table>
+        <tr>
+          <td>Balance</td>
+          <td>{{balance}}</td>
+        </tr>
+        <tr>
+          <td>Total in</td>
+          <td>{{weiToEbk(addressData.total_in)}}</td>
+        </tr>
+        <tr>
+          <td>Total out</td>
+          <td>{{weiToEbk(addressData.total_out)}}</td>
+        </tr>
+        <tr>
+          <td>Block rewards</td>
+          <td>{{weiToEbk(addressData.block_rewards)}}</td>
+        </tr>
+        <tr>
+          <td>Total txs</td>
+          <td>{{totalTx}}</td>
+        </tr>
+      </table>
+    </div>
+    <div class="panel">
+      <h2>Transactions</h2>
+      <transactions
+        v-bind:txs="txs"
+        v-bind:address="addressData.address"
+        v-bind:maxOffset="totalTx"
+        v-bind:isTransactions="{active:true }"
+      />
+    </div>
   </div>
-</div>
-    
 </template>
 
 <script>
-
-import Chart from './chart'
-import { timeConverter, weiToEbk } from '../utils';
+import Chart from "./chart";
+import { timeConverter, weiToEbk } from "../utils";
 
 export default {
-   props:{
+  props: {
     isAddressActive: {
       type: Boolean,
       default: false
     },
-    addressData:{
+    addressData: {
       type: Object
     },
-    txs:{
+    txs: {
       type: Array
     }
-   
   },
-  components:{
+  components: {
     Chart
   },
-  data () {
+  data() {
     return {
-      address:"",
-      chartDataLoaded: false,
+      address: "",
+      chartDataLoaded: false
+    };
+  },
+  methods: {
+    getBalance: function() {
+      var balance = 0;
+      var i = 0;
+      console.log("from: " + this.addressData.from.length);
+      console.log("to: " + this.addressData.space - top1.length);
+      console.log(i);
+    },
+    timeConverter: timeConverter,
+    weiToEbk: val => weiToEbk(val).toFixed(5)
+  },
+  created: function() {},
+  watch: {
+    addressData: function() {},
+    txs: function() {
+      this.chartDataLoaded = true;
     }
   },
-  methods:{
-   getBalance: function(){
-     var balance= 0
-     var i = 0;
-     console.log("from: "+this.addressData.from.length)
-     console.log("to: "+this.addressData.space-top1.length)
-     console.log(i);
-   },
-    timeConverter: timeConverter,
-    weiToEbk: (val) => weiToEbk(val).toFixed(5),
-  },
-  created: function(){
-
-
-  },
-  watch:{
-   addressData:function(){},
-   txs:function(){
-     this.chartDataLoaded = true;
-   },
-  },
-  computed:{
-    balance: function(){
-      var balance = (this.addressData.total_in - this.addressData.total_out + this.addressData.block_rewards)
-      return this.weiToEbk(balance)
-
+  computed: {
+    balance: function() {
+      var balance =
+        this.addressData.total_in -
+        this.addressData.total_out +
+        this.addressData.block_rewards;
+      return this.weiToEbk(balance);
     },
-    totalTx: function(){
-      return this.addressData.count_in + this.addressData.count_out
+    totalTx: function() {
+      return this.addressData.count_in + this.addressData.count_out;
     },
-    balanceData: function(){
-      var i
-      var balance = 0
-      var balanceData ={}
-      var _data= []
-      var _datasets= []
-      var _labels= []
-      var txs = this.txs.slice().reverse()
- 
-      for(i=0;i<txs.length;i++) {
-        _labels.push(this.timeConverter(txs[i].timestamp))
-        if(txs[i].from == "this" && txs[i].to!="this"){
-          balance -= this.weiToEbk(txs[i].value) 
+    balanceData: function() {
+      var i;
+      var balance = 0;
+      var balanceData = {};
+      var _data = [];
+      var _datasets = [];
+      var _labels = [];
+      var txs = this.txs.slice().reverse();
+
+      for (i = 0; i < txs.length; i++) {
+        _labels.push(this.timeConverter(txs[i].timestamp));
+        if (txs[i].from == "this" && txs[i].to != "this") {
+          balance -= this.weiToEbk(txs[i].value);
+        } else if (txs[i].from != "this" && txs[i].to == "this") {
+          balance += this.weiToEbk(txs[i].value);
         }
-    
-        else if(txs[i].from != "this" && txs[i].to=="this"){
-          balance += this.weiToEbk(txs[i].value)
-        }
-       
-        _data.push(balance)
+
+        _data.push(balance);
       }
-        _labels.unshift("")
-        balance = this.balance -_data[txs.length-1]
-        for(i=0;i<txs.length;i++) {
-           _data[i]+= balance
-        }
-        _data.unshift(balance)
-      
-    
+      _labels.unshift("");
+      balance = this.balance - _data[txs.length - 1];
+      for (i = 0; i < txs.length; i++) {
+        _data[i] += balance;
+      }
+      _data.unshift(balance);
+
       _datasets = [
         {
-        label: 'Balance',
-        backgroundColor: '#E4F5FD',
-        borderColor: '#31BAF3',
-        data: _data,
-      }
-      ]
+          label: "Balance",
+          backgroundColor: "#E4F5FD",
+          borderColor: "#31BAF3",
+          data: _data
+        }
+      ];
 
-      balanceData= {
+      balanceData = {
         labels: _labels,
-        datasets: _datasets,
-      }
-      
-      return balanceData
+        datasets: _datasets
+      };
 
+      return balanceData;
     }
   }
-}
+};
 </script>
-
-
-
-
-
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-table{
+table {
   text-align: left;
 }
-tr:nth-child(odd){
+tr:nth-child(odd) {
   background: #fff;
 }
 
-#block_wrapper{
-  opacity:0;
+#block_wrapper {
+  opacity: 0;
 }
-#block_wrapper.active{
-  opacity:1;
+#block_wrapper.active {
+  opacity: 1;
   display: block;
 }
-h3.address{
+h3.address {
   font-size: 22px;
 }
-.twocol{
+.twocol {
   display: inline-block;
   width: 49%;
   padding-bottom: 40px;
 }
-.twocol>span.address{
+.twocol > span.address {
   padding-left: 7px;
 }
-.twocol.right{
+.twocol.right {
   text-align: right;
 }
-.balanceLabel{
+.balanceLabel {
   font-weight: 400;
-  color: #ACB4C9;
+  color: #acb4c9;
   margin-right: 10px;
 }
-.balance{
+.balance {
   font-size: 24px;
   font-weight: 600;
 }
-.chart_wrapper{
-  position:relative;
-  width:100% !important;
-  }
+.chart_wrapper {
+  position: relative;
+  width: 100% !important;
+}
 
 @media (max-width: 560px) {
-  .twocol{
+  .twocol {
     display: block;
     width: 100%;
     overflow-x: auto;
-    padding:0px;
-    margin:0px;
-    padding:8px 0px;
-  
+    padding: 0px;
+    margin: 0px;
+    padding: 8px 0px;
   }
-  div.twocol span.address{
+  div.twocol span.address {
     font-size: 14px;
     padding-left: 0px;
-    
   }
- .twocol.right{
-  text-align: left;
-  
-  } 
-  .chart_wrapper{
+  .twocol.right {
+    text-align: left;
+  }
+  .chart_wrapper {
     padding-top: 10px;
   }
- 
-
 }
 </style>
--- src/components/tabbar.vue
+++ src/components/tabbar.vue
@@ -1,279 +1,269 @@
 <template>
-  <div id="tabbar" v-bind:class="{ active: selected }" >
+  <div id="tabbar" v-bind:class="{ active: selected }">
     <div class="scroll">
       <ul id="tabbarOptions">
-        <li id= "blocksTab" :class="{active:selected == 'blocksTab' }" v-on:click="activateTab($event,txt)" > <img src="../assets/ic_blocks.png" alt=""> Blocks</li>
-        <li id= "transactionsTab" :class="{active:selected=='transactionsTab' }" v-on:click="activateTab($event,txt)" > <img src="../assets/ic_transactions.png" alt=""> Transactions</li>
-        <li id= "statisticsTab"  :class="{active:selected=='statisticsTab'}" v-on:click="activateTab($event,txt)"><img src="../assets/ic_stats.png" alt=""> Statistics</li>
+        <li
+          id="blocksTab"
+          :class="{active:selected == 'blocksTab' }"
+          v-on:click="activateTab($event,txt)"
+        >
+          <img src="../assets/ic_blocks.png" alt> Blocks
+        </li>
+        <li
+          id="transactionsTab"
+          :class="{active:selected=='transactionsTab' }"
+          v-on:click="activateTab($event,txt)"
+        >
+          <img src="../assets/ic_transactions.png" alt> Transactions
+        </li>
+        <li
+          id="statisticsTab"
+          :class="{active:selected=='statisticsTab'}"
+          v-on:click="activateTab($event,txt)"
+        >
+          <img src="../assets/ic_stats.png" alt> Statistics
+        </li>
       </ul>
-
     </div>
-      
-    
-        <div class="container"  >
-          <blocks v-bind:isBlocks="{active:selected == 'blocksTab' }" :blocks="blocks"/>
-          <transactions v-bind:isTransactions="{active:selected == 'transactionsTab' }" :txs="txs"/>
-        </div>
-        
-      </div>
 
-
+    <div class="container">
+      <blocks v-bind:isBlocks="{active:selected == 'blocksTab' }" :blocks="blocks"/>
+      <transactions v-bind:isTransactions="{active:selected == 'transactionsTab' }" :txs="txs"/>
+    </div>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-   props: {
-      tabbarActive: {
-       type: Boolean,
-       default: false
-     },
-      selectedByRoute: {
-       type: String,
-       default: ""
-     },
-   },
-  data () {
+  props: {
+    tabbarActive: {
+      type: Boolean,
+      default: false
+    },
+    selectedByRoute: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
     return {
-      txt:"",
+      txt: "",
       isActive: false,
       tHeight: "",
-      selected:"",
+      selected: "",
       isBlocksActive: false,
       isTransactionsActive: false,
-      txs:[],
-      blocks:[]
-    }
+      txs: [],
+      blocks: []
+    };
   },
-  components:{
-    
-  },
-  methods:{
-   activateTab: function(e,selectedByRoute){
-     if(selectedByRoute!=""){
-       this.$root.$data.sharedState.contentActive = true
-       this.selected =selectedByRoute
-       this.isActive = true
-      // this.tHeight = "height: calc(100% - 215px);";
-       selectedByRoute=""
-       this.tabbarRouter()
-     }
-     else if(e && this.selected!=e.target.id) {
+  components: {},
+  methods: {
+    activateTab: function(e, selectedByRoute) {
+      if (selectedByRoute != "") {
+        this.$root.$data.sharedState.contentActive = true;
+        this.selected = selectedByRoute;
+        this.isActive = true;
+        // this.tHeight = "height: calc(100% - 215px);";
+        selectedByRoute = "";
+        this.tabbarRouter();
+      } else if (e && this.selected != e.target.id) {
+        this.selected = e.currentTarget.id;
 
-       this.selected =e.currentTarget.id
-
-       this.$root.$data.sharedState.contentActive = true
-       this.isActive = true
-      // this.tHeight = "height:calc(100% - 215px);";
-       this.tabbarRouter()
-
-
-
-     }
-     else {
-       this.selected =""
-       this.$root.$data.sharedState.contentActive = false
-       this.isActive = false
-      // this.tHeight = "height: 76px;";
-       var lastQuery = this.$root.$data.sharedState.query
-       if(lastQuery!=""){
-        this.$router.push({
-          path: '/search/' + this.$root.$data.sharedState.query
-        })
-        }
-        else {
+        this.$root.$data.sharedState.contentActive = true;
+        this.isActive = true;
+        // this.tHeight = "height:calc(100% - 215px);";
+        this.tabbarRouter();
+      } else {
+        this.selected = "";
+        this.$root.$data.sharedState.contentActive = false;
+        this.isActive = false;
+        // this.tHeight = "height: 76px;";
+        var lastQuery = this.$root.$data.sharedState.query;
+        if (lastQuery != "") {
           this.$router.push({
-          path: '/'
-        })
-     }
-
-     }
-   },
-   tabbarRouter: function(){
-      console.log(this.selected)
-       switch(this.selected){
-          case "blocksTab":
-            this.getLatestBlocks()
-            this.$router.push({
-            path: '/blocks/'
-            })
-            break
-          case "transactionsTab":
-          this.getLatestTransactions()
-            this.$router.push({
-            path: '/transactions/'
-            })
-
-            break
-          default:
+            path: "/search/" + this.$root.$data.sharedState.query
+          });
+        } else {
           this.$router.push({
-            path: '/'
-            })
-
+            path: "/"
+          });
         }
-   },
-   getLatestTransactions: function(){
+      }
+    },
+    tabbarRouter: function() {
+      console.log(this.selected);
+      switch (this.selected) {
+        case "blocksTab":
+          this.getLatestBlocks();
+          this.$router.push({
+            path: "/blocks/"
+          });
+          break;
+        case "transactionsTab":
+          this.getLatestTransactions();
+          this.$router.push({
+            path: "/transactions/"
+          });
 
-      this.$http.get(process.env.API_ENDPOINT + '/transaction/latest?limit=10&order=desc')
-        .then(function(response){
-          this.txs = response.data;
-          this.hasLoaded=true;
-        },
-        (err) => {
-          console.log(err);
-          this.hasLoaded=true;
-        });
-      console.log(this.txs)
-   },
-   getLatestBlocks: function(){
-      this.$http.get(process.env.API_ENDPOINT + '/block/-1?range=10')
-        .then(function(response){
+          break;
+        default:
+          this.$router.push({
+            path: "/"
+          });
+      }
+    },
+    getLatestTransactions: function() {
+      this.$http
+        .get(
+          process.env.API_ENDPOINT + "/transaction/latest?limit=10&order=desc"
+        )
+        .then(
+          function(response) {
+            this.txs = response.data;
+            this.hasLoaded = true;
+          },
+          err => {
+            console.log(err);
+            this.hasLoaded = true;
+          }
+        );
+      console.log(this.txs);
+    },
+    getLatestBlocks: function() {
+      this.$http.get(process.env.API_ENDPOINT + "/block/-1?range=10").then(
+        function(response) {
           this.blocks = response.data;
-          this.hasLoaded=true;
+          this.hasLoaded = true;
         },
-        (err) => {
+        err => {
           console.log(err);
-          this.hasLoaded=true;
-        });
-      console.log(this.blocks)
-   }
-    
-  },
-   created: function() {
-     if(this.selectedByRoute!=""){
-       this.activateTab(null,this.selectedByRoute)
-       this.selected =this.selectedByRoute
-     }
-  },
-  watch:{
-   tabbarActive: function(){
-    if(!this.tabbarActive){
-      this.tHeight = "height: 76px;";
-      this.selected=""
+          this.hasLoaded = true;
+        }
+      );
+      console.log(this.blocks);
     }
-   },
-  selectedByRoute: function(){
-    if(typeof this.selectedByRoute!= null)
-    this.activateTab(null,this.selectedByRoute)
-  }
-    
   },
-  computed:{
-
+  created: function() {
+    if (this.selectedByRoute != "") {
+      this.activateTab(null, this.selectedByRoute);
+      this.selected = this.selectedByRoute;
+    }
   },
-
-  
-}
+  watch: {
+    tabbarActive: function() {
+      if (!this.tabbarActive) {
+        this.tHeight = "height: 76px;";
+        this.selected = "";
+      }
+    },
+    selectedByRoute: function() {
+      if (typeof this.selectedByRoute != null)
+        this.activateTab(null, this.selectedByRoute);
+    }
+  },
+  computed: {}
+};
 </script>
-
-
-
-
-
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#tabbar{
+#tabbar {
   position: fixed;
   background: #fff;
-  left:0px;
-  right:0px;
-  bottom:0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
   height: 76px;
   /* Rectangle 3: */
-  
+
   transition: 0.35s height ease-in-out;
   /* Rectangle 3: */
 
-box-shadow: 0 2px 43px 0 rgba(172,180,201,0.40);
-
+  box-shadow: 0 2px 43px 0 rgba(172, 180, 201, 0.4);
 }
 
-#tabbarOptions{
-  width:460px;
-  margin:15px auto;
+#tabbarOptions {
+  width: 460px;
+  margin: 15px auto;
 }
-li{
+li {
   display: inline-block;
-    /* Rectangle 2: */
+  /* Rectangle 2: */
 
   border-radius: 4px;
 
-  border: 2px solid #ACB4C9;
-  color: #34393D;
+  border: 2px solid #acb4c9;
+  color: #34393d;
   padding: 8px 18px;
   margin: 0px 10px;
-  background: #F8F9FB;
+  background: #f8f9fb;
   font-weight: 600;
   -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Safari */
-     -khtml-user-select: none; /* Konqueror HTML */
-       -moz-user-select: none; /* Firefox */
-        -ms-user-select: none; /* Internet Explorer/Edge */
-            user-select: none; /* Non-prefixed version, currently
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
                                   supported by Chrome and Opera */
 }
-li.active{
-    background: #34393D;
-    color: white;
-    border: 2px solid #34393D;
+li.active {
+  background: #34393d;
+  color: white;
+  border: 2px solid #34393d;
 }
-li:hover{
-    box-shadow: 0 2px 43px 0 rgba(0,0,0,0.10);
-
+li:hover {
+  box-shadow: 0 2px 43px 0 rgba(0, 0, 0, 0.1);
 }
-li img{
-  width:14px;
+li img {
+  width: 14px;
   vertical-align: -1px;
 }
-li.active img{
-   filter: invert(100%)  grayscale(100%);
-  
+li.active img {
+  filter: invert(100%) grayscale(100%);
 }
-#tabbar.active{
+#tabbar.active {
   height: calc(100% - 215px);
 }
-#tabContentWrapper{
+#tabContentWrapper {
   height: 100%;
 
   overflow: auto;
 }
-.container{
+.container {
   margin: 0 auto;
 }
-ul.tabbarOptions li.img{
-    -webkit-user-select: none;  /* Chrome all / Safari all */
-  -moz-user-select: none;     /* Firefox all */
-  -ms-user-select: none;      /* IE 10+ */
-  user-select: none;  
+ul.tabbarOptions li.img {
+  -webkit-user-select: none; /* Chrome all / Safari all */
+  -moz-user-select: none; /* Firefox all */
+  -ms-user-select: none; /* IE 10+ */
+  user-select: none;
   cursor: pointer;
 }
 
 @media (max-width: 560px) {
-  .scroll{
-    overflow-x:auto;
+  .scroll {
+    overflow-x: auto;
   }
-  ul#tabbarOptions{
+  ul#tabbarOptions {
     width: 330px;
-    
+
     margin: 12px auto;
     padding: 0px;
-
   }
-  #tabbarOptions li{
+  #tabbarOptions li {
     padding: 5px 8px;
     margin: 0px 4px;
     font-size: 14px;
   }
-  #tabbarOptions img{
+  #tabbarOptions img {
     width: 12px;
   }
-  #tabbar{
+  #tabbar {
     height: 60px;
   }
-  #tabbar.active{
-  height: calc(100% - 100px);
-}
+  #tabbar.active {
+    height: calc(100% - 100px);
+  }
 }
 </style>
