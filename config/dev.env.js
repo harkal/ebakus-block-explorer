662fe362ced18940d797680461a4b1fd3dd8dc5a
Chris Ziogas <ziogaschr@gmail.com>
2020-01-23 17:58:22 +0200

    Clean up config for local development

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,4 @@ const prodEnv = require('./mainnet.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"https://explorersrv.ebakus.com"',
-  WEB3JS_NODE_ENDPOINT: '"ws://127.0.0.1:8546"',
 })
